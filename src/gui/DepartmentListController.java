944f4ad68f40b97e0c4b53af4e52ad0388c557a9
ckmihara <ckmihara@gmail.com>
2022-12-16 20:08:44 -0300

    The last version

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -86,7 +86,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 			throw new IllegalStateException("Service was null");
 
 		}
-		List<Department> list = service.finAll();
+		List<Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
 		initEditButtons();
